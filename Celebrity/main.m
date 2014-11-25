04c6293082950a514cfba46c149aa959debe44e2
Chris Echanique <cechanique@trulia.com>
2014-11-24 21:15:57 -0800

    Initial Commit

---
+++ Celebrity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Celebrity
+//
+//  Created by Christopher Echanique on 11/24/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
